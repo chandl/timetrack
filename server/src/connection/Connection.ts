f6afc62981f5a0892478c3f3d8ecc1f918c07e17
Chandler <me@chandl.io>
2021-03-19 15:22:14 -0700

    Add connection retry logic for database

--- server/src/connection/Connection.ts
+++ server/src/connection/Connection.ts
@@ -1,2 +1,27 @@
 import { createConnection } from "typeorm";
-export const connection = createConnection();
+
+const MAX_RETRIES = 10;
+const RETRY_DELAY = 500;
+
+const getConnection = async () => {
+    let delay = RETRY_DELAY;
+    let conn;
+    for (let i = 0; i < MAX_RETRIES; i++) {
+        let error;
+        createConnection().then(c => conn = c).catch(err => error = err);
+        if ( conn ) break;
+        console.log(`Failed to connect to database, retrying (attempt ${i + 1}/${MAX_RETRIES}) in ${delay}ms`);
+        await new Promise(r => setTimeout(r, delay));
+        delay += RETRY_DELAY
+    }
+
+    if (!conn) {
+        console.error("FATAL: Failed to connect to the database. Exiting");
+        process.exit(-1);
+    }
+
+    console.log("Successfully connected to the database")
+    return conn;
+}
+
+export const connection = getConnection();
